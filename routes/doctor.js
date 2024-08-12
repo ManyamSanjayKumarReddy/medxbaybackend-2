26e67a654c8c8567158e0f4287a916d6b69ef4e4
Loga priya Eswaran <logapriyaeswaran@Logas-MacBook-Air.local>
2024-08-12 16:05:41 +0530

    added doctor

--- routes/doctor.js
+++ routes/doctor.js
@@ -30,11 +30,13 @@ router.use(methodOverride('_method'));
 
 function isLoggedIn(req, res, next) {
     if (req.session && req.session.user) {
+        req.user=req.session.user
       return next();
     } else {
       res.status(401).json({ success: false, message: 'Unauthorized access attempt.' });
     }
   }
+  
   
   
 
