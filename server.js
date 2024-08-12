f2fda2122ba447d4800341f3e2e1c92612d09aa6
Loga priya Eswaran <logapriyaeswaran@Logas-MacBook-Air.local>
2024-08-12 21:39:27 +0530

    added cta for corporates

--- server.js
+++ server.js
@@ -21,12 +21,12 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.use(cors({
-  origin: 'http://localhost:3000', 
-  methods: 'GET,POST,PUT,DELETE',
-  allowedHeaders: 'Content-Type,Authorization',
-  credentials: true 
-
+  origin: ['http://localhost:3000', 'https://medxbay-deploy-1-431103.uc.r.appspot.com'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
 }));
+
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
